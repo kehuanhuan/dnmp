8a8890edafa66c424b04721a942f486eae3873a8
khh <kehh>
2025-11-19 22:21:07 +0800

    min fix

--- services/php/extensions/install.sh
+++ services/php/extensions/install.sh
@@ -195,7 +195,7 @@ fi
 if [[ -z "${EXTENSIONS##*,gd,*}" ]]; then
     echo "---------- Install gd ----------"
     apk add --no-cache freetype-dev libjpeg-turbo-dev libpng-dev \
-    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
     && docker-php-ext-install ${MC} gd
 fi
 
